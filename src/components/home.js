b5adf8f7c140b792a503ad554ad584f7f0d80b55
Bmleguizamon <brunomleguizamon@gmail.com>
2021-04-28 19:08:12 -0300

    change infomation

--- src/components/home.js
+++ src/components/home.js
@@ -6,6 +6,36 @@ import {
   AiFillLinkedin,
 } from "react-icons/ai";
 
+function navMenu() {
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach((e) => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
+
+  on("click", ".mobile-nav-toggle", function (e) {
+    select("#navbar").classList.toggle("navbar-mobile");
+    this.classList.toggle("bi-list");
+    this.classList.toggle("bi-x");
+    console.log("ouch from home");
+  });
+}
+
 function Home() {
   return (
     <header id="header">
@@ -44,7 +74,11 @@ function Home() {
               </a>
             </li>
           </ul>
-          <BsList className="bi bi-list mobile-nav-toggle" color="#c70039" />
+          <BsList
+            onClick={navMenu}
+            className="bi bi-list mobile-nav-toggle"
+            color="#c70039"
+          />
         </nav>
         <div className="social-links">
           <a href="https://github.com/Brunomleguizamon" className="github">
--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,6 +1,45 @@
 import React from "react";
 import { BsList } from "react-icons/bs";
 
+function navMenu() {
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach((e) => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
+
+  /**
+   * Mobile nav toggle
+   */
+  on("click", ".mobile-nav-toggle", function (e) {
+    select("#navbar").classList.toggle("navbar-mobile");
+    this.classList.toggle("bi-list");
+    this.classList.toggle("bi-x");
+    console.log("ouch from nav-bar");
+  });
+}
+
 function navBar() {
   return (
     <header id="header" class="header-top">
@@ -36,7 +75,11 @@ function navBar() {
               </a>
             </li>
           </ul>
-          <BsList className="mobile-nav-toggle bi bi-list" color="#c70039" />
+          <BsList
+            onClick={navMenu}
+            className="mobile-nav-toggle bi bi-list"
+            color="#c70039"
+          />
         </nav>
       </div>
     </header>
